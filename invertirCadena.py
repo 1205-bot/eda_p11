fdcb83ac0063db72a595d3c9c2fdc948f68b3f18
Ramirez Olvera Erick Yael <ypollo20@gmail.com>
2026-05-07 16:31:21 +0000

    Subo invertirCadena.py

---
+++ invertirCadena.py
@@ -0,0 +1,27 @@
+#Diseña un algoritmo recursivo que imprima una cadena al reves 
+
+#A) Escribe el caso base: cuando i == 0
+#B) Escribe el caso recursivo: i - 1
+#C) Encuentra la ecuación de recurrencia 
+#D) Encuentra la complejidad del algoritmo usando la ecuación de recurrencia 
+
+i = 4
+def imp_arr(arr, i):
+    if i == 0:
+        return
+    
+    print(arr[i])
+
+    imp_arr(arr, i - 1)
+
+arr = [10, 20, 30, 40, 50]
+
+imp_arr(arr, i)
+
+#A) i == 0
+#B) i - 1
+#C) T(n) = T(n - 1) + 1
+#D) T(n) + T(n - 1) = 1
+#   si T(n) = x^1
+#   (x - 1)(x - 1) = 0
+#   O(T(n)) = n
